d549cdfc48ed123aefdbdca4e36dea280b45bd3a
wing-p <wing@hashtag.com.sg>
2017-01-09 00:54:21 +0800

    Update enqueuejsscript.js

--- enqueuejsscript.js
+++ enqueuejsscript.js
@@ -1,3 +1,14 @@
-add_action( 'wp_enqueue_scripts', 'wing_enqueue_custom' );
-function wing_enqueue_custom() {
-		wp_enqueue_script( 'customscript', get_stylesheet_directory_uri() . '/js/customscript.js');
+add_action( 'wp_enqueue_scripts', 'wing_enqueue_scripts' );
+function wing_enqueue_scripts() {
+	wp_enqueue_script( 
+	//script name// 
+	'customscript',  
+	//location//  
+	get_stylesheet_directory_uri() . '/js/customscript.js', 
+	//define dependency file// 
+	array(''), 
+	//vers//
+	'1.0', 
+	//boolean for loading js script at footer//
+	true);
+}
