7a6d771227ae7ee89de806efadc1736691ed1de0
wing-p <wing@hashtag.com.sg>
2017-01-14 16:09:19 +0800

    Add files via upload

---
+++ starterfunctionforgenesis.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Genesis Sample.
+ *
+ * This file adds functions to the Genesis Sample Theme.
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0+
+ * @link    http://www.studiopress.com/
+ */
+
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Setup Theme
+include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
+
+//* Set Localization (do not remove)
+load_child_theme_textdomain( 'hashtag', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'hashtag' ) );
+
+//* Add Image upload and Color select to WordPress Theme Customizer
+require_once( get_stylesheet_directory() . '/lib/customize.php' );
+
+//* Include Customizer CSS
+include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Hashtag' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_VERSION', '2.2.4' );
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+
+//* Add Accessibility support
+add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Add Image Sizes
+add_image_size( 'featured-image', 720, 400, TRUE );
+
+//*Start customisation
+//* Load custom.css
+add_action( 'wp_enqueue_scripts', 'custom_load_custom_style_sheet' );
+function custom_load_custom_style_sheet() {
+ wp_enqueue_style( 'custom-stylesheet', CHILD_URL . '/custom.css', array(), PARENT_THEME_VERSION );
+}
+
+//* Add support for structural wraps
+add_theme_support( 'genesis-structural-wraps', array(
+    'header',
+    'menu-primary',
+) );
